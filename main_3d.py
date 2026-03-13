45c1467e4b175a22235d50d616254e0d4fe029ba
JANGA KOMAL REDDY <128179020@sastra.ac.in>
2026-03-14 01:41:58 +0530

    Remove tracked pycache binaries and ignore Python bytecode

---
+++ main_3d.py
@@ -0,0 +1,10 @@
+"""
+Run the non-Pygame 3D prototype:
+    pip install ursina
+    python main_3d.py
+"""
+from game3d.app import Cricket3DApp
+
+
+if __name__ == '__main__':
+    Cricket3DApp().run()
